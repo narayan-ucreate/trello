fc718b2f39e127e82c469396452bc3ead3ceae57
Narayan Sharma <narayansharma@ucreate.co.in>
2019-02-04 13:26:32 +0530

    add configuration for production

--- src/config.js
+++ src/config.js
@@ -5,7 +5,11 @@ const config = {
         port : 4000,
         trello_api_key : process.env.TRELLO_API_KEY,
         trello_token : process.env.TRELLO_TOKEN
+    },
+    production: {
+        port : 4000,
+        trello_api_key : process.env.TRELLO_API_KEY,
+        trello_token : process.env.TRELLO_TOKEN
     }
-
 };
 module.exports = config[environment]
