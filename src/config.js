d9632c60dae9db202b3fb4f0d743e8c949415462
Narayan Sharma <narayansharma@ucreate.co.in>
2019-02-04 15:13:50 +0530

    update port number

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const environment = process.env.APP_ENV || 'developement'
 const config = { 
     local: {
-        port : 4000,
+        port : process.env.PORT || 4000,
         trello_api_key : process.env.TRELLO_API_KEY,
         trello_token : process.env.TRELLO_TOKEN,
         environment
     },
     production: {
-        port : 4000,
+        port : process.env.PORT || 4000,
         trello_api_key : process.env.TRELLO_API_KEY,
         trello_token : process.env.TRELLO_TOKEN,
         environment
